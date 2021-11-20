7da4f8a24fa8b352997cb9cef14768e8cef104eb
brunoPassosOliveira <bpassos35@gmail.com>
2021-11-20 18:41:42 +0000

    Classes abstratas e Polimorfismo

---
+++ Exercicio Fixacao (Contribuintes)/src/entities/Individual.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Individual extends TaxPayer {
+	
+	private Double healthExpenditures;
+
+	
+	public Individual() {
+		
+	}
+		
+	public Individual(String name, Double anualIncome, Double healthExpenditures) {
+		super(name, anualIncome);
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	public Double getHealthExpenditures() {
+		return healthExpenditures;
+	}
+
+	public void setHealthExpenditures(Double healthExpenditures) {
+		this.healthExpenditures = healthExpenditures;
+	}
+
+	@Override
+	public Double tax() {
+		if(getAnualIncome()<20000.00) {
+			return (getAnualIncome() * 15/100)-getHealthExpenditures()/2;
+		}else {
+			return (getAnualIncome() * 25/100)-getHealthExpenditures()/2;
+		}
+	}
+	
+
+
+}
